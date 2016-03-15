283b45e08d6fa84a3784b811c2db70b3f068b3b0
Joseph Koshakow <koshy44@gmail.com>
2016-03-14 20:24:38 -0400

    first comit

---
+++ app/src/main/java/hu/ait/android/moneyconverter/data/MoneyResult.java
@@ -0,0 +1,53 @@
+package hu.ait.android.moneyconverter.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by joe on 11/26/15.
+ */
+public class MoneyResult {
+
+    @SerializedName("base")
+    @Expose
+    private String base;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("rates")
+    @Expose
+    private Rates rates;
+
+    public MoneyResult(String base, String date, Rates rates) {
+
+        this.base = base;
+        this.date = date;
+        this.rates = rates;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Rates getRates() {
+        return rates;
+    }
+
+    public void setRates(Rates rates) {
+        this.rates = rates;
+    }
+
+
+}
